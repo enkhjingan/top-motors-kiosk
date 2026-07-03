02edf8a91b96664cdaf3b087d015d3cf46594053
enkhjin.g <hanna@hannas-MacBook-Pro.local>
2026-07-03 15:54:08 +0800

    Initial React + Vite setup

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+    return (
+        <h1>Top Motors Kiosk</h1>
+    );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+{/*import styles*/}
+import './styles/variables.css';
+import './styles/globals.css';
+import './styles/typography.css';
+import './styles/animations.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
